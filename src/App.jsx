f5c89beb212cbf39c580bb796a205dd91a3aaa4c
HoangTrinhyeuoi <hoangtrinh240705@gmail.com>
2025-08-15 01:45:26 +0700

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import LandingPage from './pages/LandingPage';
@@ -10,13 +10,15 @@ import './App.css';
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/products" element={<ProductListingPage />} />
-          <Route path="/cart" element={<ShoppingCartPage />} />
-        </Routes>
-      </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/products" element={<ProductListingPage />} />
+            <Route path="/cart" element={<ShoppingCartPage />} />
+          </Routes>
+        </div>
+      </Router>
     </Provider>
   );
 }
