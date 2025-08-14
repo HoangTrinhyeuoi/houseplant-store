9f24c2ee654bb14ea4ccf0114a50dad8c41f4a8b
HoangTrinhyeuoi <hoangtrinh240705@gmail.com>
2025-08-15 01:57:33 +0700

    Fix Redux Provider and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import ProductListingPage from './pages/ProductListingPage';
 import ShoppingCartPage from './pages/ShoppingCartPage';
 import './App.css';
@@ -9,9 +10,9 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<ProductListingPage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/products" element={<ProductListingPage />} />
           <Route path="/cart" element={<ShoppingCartPage />} />
-          {/* Add other routes here */}
         </Routes>
       </div>
     </Router>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </Provider>
+  </StrictMode>,
 )
